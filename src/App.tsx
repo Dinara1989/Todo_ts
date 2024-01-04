a937ad0008b6c84795d88940927fc473212bd456
Dinara <dinara198989@bk.ru>
2024-01-04 15:32:52 +0100

    microtasks, button, Handler

--- src/App.tsx
+++ src/App.tsx
@@ -17,19 +17,19 @@ const App = () => {
     //     console.log('Hello, Im Hro')
     // }
 
-    // const foo1=()=> {
-    //     console.log(100200)
-    // }
-    // const foo2=(num:number)=> {
-    //     console.log(num)
-    // }
+    const foo1=()=> {
+        console.log(100200)
+    }
+    const foo2=(num:number)=> {
+        console.log(num)
+    }
     const onClickHandler = (name:string) => {
         console.log(name)
     }
     return (
         <div>
-            {/*<button onClick={foo1}>1</button>*/}
-            {/*<button onClick={() => foo2(10020)}>2</button>*/}
+            <button onClick={foo1}>1</button>
+            <button onClick={() => foo2(10020)}>2</button>
             <button onClick={(event) => onClickHandler('Dinara')}> My YouTube Chanel 1</button>
             <button onClick={(event) => onClickHandler('Hro')}>My YouTube Chanel 2</button>
             <Header title={'First'}/>
